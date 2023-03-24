ec3233aa23802c6c012bbc4dbcb7dfbfebbaf71d
Twin J <junohanyere@gmail.com>
2023-03-24 21:48:45 +0100

    Still not resolved, progressing though

--- test.c
+++ test.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
+int _isnumber(char str[])
+{
+	/*char str;*/
+
+	printf(isalpha(str[1]));
+
+}
 
 int main(void)
 
 {
 
-	char str[1000];
+	char s[1000];
 
 	printf("Hi >>");
-	scanf("%s", str);
+	scanf("%d", s);
 
-	int length = strlen(str);
-
-	printf("Hey %d", length);
+	printf("Hey %d", _isnumber);
 
 	return (0);
 
