22cf63e4bbf032cb68a402fbe70fc9185b64b625
Twin J <junohanyere@gmail.com>
2023-03-25 14:08:41 +0100

    Resolved errors in do_hard_things.c

--- do_hard_things.c
+++ do_hard_things.c
@@ -1,34 +1,12 @@
 /* Importing the necessary Libraries*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
-/* _isnumber - User defined function
- *
- * Description: Checks and validates if input is contains strings or integer
- *
- * Return: Return 0 if input is integer, and 1 if input contains string
-*/
-
-int _isnumber(char str)
-{
-	for (int i = 0; i < strlen(str); i++)
-	{
-		if ((isalpha(str[i]) != 0) || (str[i] == '\0' || "\n"))
-		{
-			break;
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
-	}
-	return (0);
-}
-
-/* questions - User defined fuction, entry for questions
+/**
+ * questions - User defined fuction, entry for questions
  *
  * Description: Contains the questions to be answered
  *
@@ -38,6 +16,37 @@ int _isnumber(char str)
 int questions(void)
 {
 	/* questions here */
+	printf("\n Questions under construction");
+}
+
+/**
+ * _isnumber - User defined function
+ *
+ * @str: String to check and validate
+ *
+ * Description: A function to check an validate if input contains letter or not
+ *
+ * Return: Return 1 or 2
+*/
+
+int _isnumber(char str[])
+{
+	int n;
+	int length = strlen(str);
+
+	for (int i = 0; i < length; i++)
+	{
+		if (isalpha(str[i]) != 0)
+		{
+			break;
+			n = 1;
+		}
+		else
+		{
+			n = 2;
+		}
+	}
+	return (n);
 }
 
 /**
@@ -50,42 +59,32 @@ int questions(void)
 
 int main(void)
 {
-
 	printf("Welcome To Do_Hard_Things\n");
 	printf("Try to answer the questions YOURSELF, ");
 	printf("so you can know how much your improving.");
 	printf("YOU ARE NOT FORCED!\n");
 
-	while (1)
+	printf("\nEnter the desired number of questions you want to answer. ");
+	printf("The recommended is also the minimum you can answer, which is 10 >> ");
+
+	int num_questions, length;
+	char str[1000];
+
+	scanf("%[^\n]s", str);
+
+	int isnumber = _isnumber(str);
+
+	if (isnumber == 2)
 	{
-
-		printf("\nEnter the desired number of questions you want to answer. ");
-		printf("The recommended is also the minimum you can answer, which is 10 >> ");
-
-		int num_questions, length;
-		char n[100];
-
-		scanf("%s", n);
-		int  _isnumber = _isnumber(n);
-
-		if (_isnumber == ("0" * strlen(n)))
-		{
-			num_questions = atoi(n);
-			if (num_questions >= 10)
-
-			{
-				questions();
-			}
-			else
-			{
-				printf("\n Expected numbers starting from 10\n");
-			}
+		num_questions = atoi(str);
+		if (num_questions >= 10)
+			questions();
 		else
-		{
-			printf("\n Expected numbers starting from 10\n");
-		}
-
-
+			printf("\n Invalid input");
+	}
+	else
+	{
+		printf("Wrong");
 	}
 
 	return (0);
