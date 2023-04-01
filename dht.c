5f3effc9d1ab6b74e545eefd8e304a08e2b69691
Twin J <junohanyere@gmail.com>
2023-04-01 15:37:46 +0100

    Updated dht.c

--- dht.c
+++ dht.c
@@ -18,9 +18,9 @@ int main(void)
 	printf("Welcome To Do_Hard_Things\n");
 	printf("Try to answer the questions YOURSELF, ");
 	printf("so you can know how much your improving.");
-	printf("YOU ARE NOT FORCED!\n");
+	printf("YOU ARE NOT FORCED!\n\n");
 
-	printf("\nEnter the desired number of questions you want to answer. ");
+	printf("Please enter your desired number of questions. ");
 	printf("The recommended is also the minimum you can answer, which is 10 >> ");
 
 	int num_questions, length;
@@ -30,20 +30,42 @@ int main(void)
 
 	int isnumber = _isnumber(str);
 
-	if (isnumber == 0)
+	if (isnumber == 1)
 	{
 		/* convert the string to integer */
 		num_questions = atoi(str);
 
 		if (num_questions >= 10)
+		{
+			for (int i = 0; i <= num_questions; i++)
+			{
+				const char *question = questions(_rand(range), range);
+				printf("%[^\n]s", question);
+
+				char ans[1];
+				scanf("%s", ans);
+
+				/**
+				 * if (_rand(range) = 1 || 5 || 8 || ...)
+				{
+					if (ans == a)
+						printf("You got that right")
+					else
+						printf("Oops! The right answer is a")
+				}
+				 * Creating a separate fuction for this would be preferable
+				 * Especially in a separate file
+				*/
+		}
 			printf("\n Oops!! Question under construction");
 		else
+
 			printf("\n Invalid input");
 	}
 	else
-	{
+
 		printf("\n Invalid input");
-	}
+
 
 	return (0);
 
