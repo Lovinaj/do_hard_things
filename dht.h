16544a0641bb0ac8a03c97f9bb1fc206f2ff10fb
Twin J <junohanyere@gmail.com>
2023-04-02 15:45:34 +0100

    Added prototypes for dht_answers.c file, grouped and added comments to each of the prototype grouping of a file

--- dht.h
+++ dht.h
@@ -3,8 +3,20 @@
 
 #include <stdio.h>
 
+/* Prototypes for dht_questions.c */
+
 const char *question(int num, int range);
+
+/* Prototypes for dht_random.c */
+
 int _isnumber(char str[]);
 int _rand(int range);
+
+/* Prototypes for dht_answers.c */
+
+void options_a(void);
+void options_b(void);
+void options_c(void);
+void options_d(void);
 
 #endif /* DHT_H */
