45c3a9847ce9ac62bc71b82b92cf77297503680f
Twin J <junohanyere@gmail.com>
2023-04-09 17:13:03 +0100

    Added two new functions

--- dht_random.c
+++ dht_random.c
@@ -67,33 +67,87 @@ int _rand(int range)
 	return (rand() % range);
 }
 
-int questions_to_answer(void)
+void questions_to_answer(void)
 {
-	int num_questions, flag;
-	char str[1000];
-
-	flag = scanf("%s", str);
-
-	if (flag == EOF)
-		exit(EXIT_SUCCESS);
-
-	int isnumber = _isnumber(str);
-
-	if (isnumber == 1)
+	while (1)
 	{
-		/* convert the string to integer */
-		num_questions = atoi(str);
+		printf("Please enter your desired number of questions. ");
+		printf("The recommended is also the minimum you can answer, ");
+		printf("which is 10 >> ");
 
-		if (num_questions >= 10) /* Minimum required is 10 questions */
+		int num_questions, flag;
+		char str[1000];
+
+		flag = scanf("%s", str);
+
+		if (flag == EOF) /**
+				  * checks if ctrl-D is entered
+				  * if so, the program exits
+				 */
+
+			exit(EXIT_SUCCESS);
+
+		int isnumber = _isnumber(str);
+
+		if (isnumber == 1)
 		{
-			printf(" You Requested To Answer %d Questions\n", num_questions);
-			printf("\n");
+			/* convert the string to integer */
+			num_questions = atoi(str);
 
+			if (num_questions >= 10) /* Minimum required is 10 questions */
+			{
+				printf(" You Requested To Answer %d Questions\n", num_questions);
+				printf("\n");
+
+				show_questions(num_questions);
+			}
+			else
+				printf("\n Invalid input\n\n");
 		}
 		else
 			printf("\n Invalid input\n\n");
 	}
-	else
-		printf("\n Invalid input\n\n");
-	return (0);
+}
+
+void show_questions(int num)
+{
+	int score = 0, flag;
+	char *pass = "Passed";
+	char *fail_a = "Failed [a]";
+	char *fail_b = "Failed [b]";
+	char *fail_c = "Failed [c]";
+
+	for (int i = 0; i < num; i++)
+	{
+		int range = 6, random = _rand(range), j = i + 1;
+
+		/* Display a question */
+		const char *_question = question(random);
+
+		printf("\n");
+		printf("%d. %s\n\n ", j, _question);
+
+		/* creating a string variable to accept answers from user */
+		char ans[1];
+
+		flag = scanf("%s", ans);
+
+		if (flag == EOF)
+			exit(EXIT_SUCCESS);
+
+		/* validate if the answer is correct */
+		const char *validator = answers(random, ans);
+
+		printf(" %s\n", validator);
+		if (strcmp(validator, pass) == 0)
+			score = score + 1;
+		else if (strcmp(validator, fail_a) == 0
+		|| strcmp(validator, fail_b) == 0 || strcmp(validator, fail_c) == 0)
+			score = score - 1;
+		else
+			score = score;
+	}
+	float percentage = ((float) score / num) * 100;
+
+	printf("%d\n%.2f\n%d\n", score, percentage, num);
 }
