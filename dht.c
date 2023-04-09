0d32e3600deaffc59e5e1ab8f08784409188227e
Twin J <junohanyere@gmail.com>
2023-04-09 17:12:31 +0100

    Finally resolve betty coding style, 'No more than 40 lines in a function

--- dht.c
+++ dht.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "dht.h"
 
@@ -22,72 +23,7 @@ int main(void)
 	printf("Also note that you will be penalized ");
 	printf("for wrong answers\n\n");
 
-	int flag; /**
-		   * Variable to check whether ctrl-D is entered
-		   * If ctrl-D is entered, the program exits
-		   * Also used to catch the EOF resulting from ctrl-D
-		   * which results in infinite loop
-		  */
-
-	while (flag != EOF)
-	{
-		printf("Please enter your desired number of questions. ");
-		printf("The recommended is also the minimum you can answer, ");
-		printf("which is 10 >> ");
-
-		int num_questions, length;
-		char str[1000];
-
-		flag = scanf("%s", str);
-
-		if (flag == EOF)
-			exit(0);
-
-		int isnumber = _isnumber(str);
-
-		if (isnumber == 1)
-		{
-			/* convert the string to integer */
-			num_questions = atoi(str);
-
-			if (num_questions >= 10) /* Minimum required is 10 questions */
-			{
-				printf(" You Requested To Answer %d Questions\n", num_questions);
-				printf("\n");
-
-				for (int i = 0; i < num_questions; i++)
-				{
-					int range = 6, random = _rand(range), j = i + 1;
-
-					/* Display a question */
-					const char *quest = question(random);
-
-					printf("\n");
-					printf("%d. %s\n\n ", j, quest);
-
-					/* Creating a string variable to accept answer from user */
-					char ans[1];
-
-					flag = scanf("%s", ans);
-
-					if (flag == EOF)
-						exit(0);
-
-					/* Check if the answer is correct */
-					const char *checker = answers(random, ans);
-
-					printf(" %s\n", checker);
-				}
-			}
-
-			else
-
-				printf("\n Invalid input\n\n");
-		}
-		else
-
-			printf("\n Invalid input\n\n");
-	}
+	questions_to_answer();
 
 	return (0);
 }
