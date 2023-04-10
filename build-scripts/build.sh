d12225e96a9267b0340b3bf7361e24ab66388a6f
Twin J <junohanyere@gmail.com>
2023-04-10 20:59:51 +0100

    Modified

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc ../dht/dht.c ../lib/dht_lib.c ../questions/dht_questions_c.c ../answers/dht_answers_c.c -o ../dht.out
+gcc ../dht/dht.c ../lib/dht_lib.c ../questions/dht_questions_c.c ../answers/dht_answers_c.c -o ../dht/dht.out
