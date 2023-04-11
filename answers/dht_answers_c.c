5b70040648d48ce62ed047d7e6c25184cd823cc3
Twin J <junohanyere@gmail.com>
2023-04-11 02:16:37 +0100

    Added more indexes with respect to the questions

--- answers/dht_answers_c.c
+++ answers/dht_answers_c.c
@@ -18,10 +18,10 @@ const char *answers(int num, char str[])
 	if ((num == 0 || num == 1 || num ==  3)
 	&& strcmp(str, "a") == 0)
 		return ("\033[0;32mPassed\033[0m");
-	else if ((num == 2 || num == 4)
+	else if ((num == 2 || num == 4 || num == 9 )
 	&& strcmp(str, "b") == 0)
 		return ("\033[0;32mPassed\033[0m");
-	else if ((num == 5 || num == 6)
+	else if ((num == 5 || num == 6 || num == 7 || num == 8 || num == 10)
 	&& strcmp(str, "c") == 0)
 		return ("\033[0;32mPassed\033[0m");
 	else if (strcmp(str, "d") == 0)
@@ -34,7 +34,7 @@ const char *answers(int num, char str[])
 	else
 		if (num == 0 || num == 1 || num == 3)
 			return ("\033[0;31mFailed \033[0;32m[a]\033[0m");
-		else if (num == 2 || num == 4)
+		else if (num == 2 || num == 4 || num == 9)
 			return ("\033[0;31mFailed \033[0;32m[b]\033[0m");
 		else
 			return ("\033[0;31mFailed \033[0;32m[c]\033[0m");
